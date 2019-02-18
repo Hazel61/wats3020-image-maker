291091b853319353f7f5d5114558b72d4634b43e
Shannon Michaels <michaelsshan@seattleu.edu>
2019-02-17 17:48:43 -0800

    Second commit. Fixed bugs, works now.

--- js/main.js
+++ js/main.js
@@ -14,22 +14,23 @@ class ImageMaker {
         // When this class is instantiated, the `constructor()` method is executed.
         // TODO: Set up attributes that point to the HTML elements we wish to work with.
 
+
         // TODO: Select the `#image-preview` div using any document selector method.
         this.imagePreview = document.querySelector("#image-preview");
 
         // TODO: create a new `<p>` element called `this.topText`
         this.topText = document.createElement('p');
         // TODO: Add a `class` attribute to `this.topText` that contains the classname "top-text".
-        this.topText = document.setAttribute('class', 'top-text');
+        this.topText.setAttribute('class', 'top-text');
         // TODO: Append `this.topText` as a child element to `this.imagePreview`
 
-        this.imagePreview.appendChild("this.topText");
+        this.imagePreview.appendChild(this.topText);
         // TODO: create a new `<p>` element called `this.bottomText`
         this.bottomText = document.createElement("p");
         // TODO: Add a `class` attribute to `this.bottomText` that contains the classname "bottom-text".
-        this.bottomText = document.setAttribute('class', 'bottom-text');
+        this.bottomText.setAttribute('class', 'bottom-text');
         // TODO: Append `this.bottomText` as a child element to `this.imagePreview`
-        this.imagePreview.appendChild("this.bottomText")
+        this.imagePreview.appendChild(this.bottomText);
         // This class also needs to use the form fields to read user input. Set
         // those up for future use, too.
 
@@ -37,10 +38,10 @@ class ImageMaker {
         this.backgroundInput = document.forms[0].querySelector('select[name = "backgroundImage"]');
 
         // TODO: Select the `input` element with the `name` attribute "topText"
-        this.topTextInput = document.forms[0].querySelector('input[name = "topText"]');
+        this.topTextInput = document.forms[0].querySelector('input[name = "topText"');
 
         // TODO: Select the `input` element with the `name` attribute "bottomText"
-        this.bottomTextInput = document.forms[0].querySelector('input[name = "bottomText"]');
+        this.bottomTextInput = document.forms[0].querySelector('input[name = "bottomText"');
 
         // NOTE: If you add additional form fields to modify other aspects of
         // the image, then you will need to make attributes for each of those
